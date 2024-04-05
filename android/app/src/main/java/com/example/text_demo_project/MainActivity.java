64a96020547e511ff91a14991668b475eaef33ab
megha238 <megha121panchal@gmail.com>
2024-04-05 15:04:58 +0530

    align add

---
+++ android/app/src/main/java/com/example/text_demo_project/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.text_demo_project;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/textfile_demo.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class TextFileDemo extends StatefulWidget {
+  const TextFileDemo({super.key});
+
+  @override
+  State<TextFileDemo> createState() => _TextFileDemoState();
+}
+
+class _TextFileDemoState extends State<TextFileDemo> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:Text('megha',textAlign: TextAlign.center) ,
+    );
+  }
+}
